624384dda4f26d72259ea11505d03b0124cc115b
茨城大学航空技術研究会 <ibarakikougiken@gmail.com>
2025-03-05 16:20:56 +0900

    Restructure header component (#1)
    * Restructure header component
    
    * Header component: adjust z-index for better stacking context
    
    * Use grid and improve responsiveness

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
+
+/**
+ * @see https://pagefind.app/docs/ui/
+ */
+declare module "@pagefind/default-ui" {
+  declare class PagefindUI {
+    constructor(opts: { element?: string | HTMLElement });
+  }
+}
