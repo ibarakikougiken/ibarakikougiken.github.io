43da2bde4cd2d5eea06d5b29d9ab964a2c3da5a7
ibarakikougiken <ibarakikougiken@gmail.com>
2025-03-07 21:27:51 +0900

    Add gtag

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, sharpImageService } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +14,16 @@ export default defineConfig({
   devToolbar: {
     enabled: false,
   },
-  integrations: [icon(), mdx(), sitemap()],
+  integrations: [
+    icon(),
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   vite: {
     css: {
       transformer: "lightningcss",
